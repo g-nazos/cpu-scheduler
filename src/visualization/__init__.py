07c02fad014241ecbfcf14780527309c7228af61
George Nazos <geonazos@hotmail.com>
2026-02-19 00:17:21 +0200

    Stop tracking __pycache__, remove from repo

--- src/visualization/__init__.py
+++ src/visualization/__init__.py
@@ -3,6 +3,7 @@
 from src.visualization.plots import (
     plot_price_evolution,
     plot_allocation_timeline,
+    plot_allocation_and_prices,
     plot_solution_value_comparison,
     plot_epsilon_sensitivity,
     plot_convergence_trace,
@@ -11,6 +12,7 @@ from src.visualization.plots import (
 __all__ = [
     "plot_price_evolution",
     "plot_allocation_timeline",
+    "plot_allocation_and_prices",
     "plot_solution_value_comparison",
     "plot_epsilon_sensitivity",
     "plot_convergence_trace",
